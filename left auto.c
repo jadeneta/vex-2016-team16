0a8df5dfb1f5b53d3c731728f660664207e4f928
jadeneta <jadenmorales@gmail.com>
2017-02-13 16:51:29 -0500

    added the other auto in different files

---
+++ left auto.c
@@ -0,0 +1,16 @@
+void autoleft() {
+
+	moveForwardWithSensor(640);
+	turnRightWithSensor(260);
+	moveForwardWithSensor(450);
+	closeclaw();
+	wait1Msec(500);
+	liftarm();
+	waitforarmheight(2500);
+	turnLeftWithSensor(200);
+	moveForwardWithSensor(750);
+	openclaw();
+	moveBackwardWithSensor(300);
+	turnLeftWithSensor(480);
+
+}
