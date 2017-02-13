870c3eaffacfd308c85c4bfae78c7e263197c1ac
jadeneta <jadenmorales@gmail.com>
2017-02-13 16:46:00 -0500

    ver 2.2.1
    added a new file for right auto to reduce the size. figured out that the
    lift problem was the battery

--- team 16 competition program.c
+++ team 16 competition program.c
@@ -542,39 +542,14 @@ void autoskill() {
 
 	// 2400
 }
-void autoright() {
-	clawtarget = 2300;
-
-
-	moveForwardWithSensor(600);
-
-	wait1Msec(500);
-	turnLeftWithSensor(200)
-	moveForwardWithSensor(400);
-	closeclaw();
-	wait1Msec(500);
-	liftarm();
-	waitforarmheight(2500);
-	turnRightWithSensor(130);
-	moveForwardWithSensor(850);
-	clawtarget = 2300;
-	wait1Msec(1000);
-	moveBackwardWithSensor(600);
-	armtarget = 2400;
-	openclaw();
-	//turnWithSensor(60);
-	wait1Msec(500);
-	moveForwardWithSensor(650);
-	wait1Msec(300);
-	moveBackwardWithSensor(650);
-	droparm();
 
 
 
 
+#include "right auto.c"
 
 
-}
+
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //
